d0d03a18faf9160c84bdf3bc6250f48ddf2b2272
Deepak K Anand <deepak.kanand@ymail.com>
2016-01-03 11:36:38 +0530

    Re-factored the Constructor
    [i] A more structured way of sending parameters to the Constructor
    [ii] Add a a couple more options such as "appendTable" and "caption"
    [iii] Cell Renderer will now receive two params: isColHeader and isRowHeader.

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,6 @@
 /**
  * @author          Deepak K Anand
- * @version         1.0
+ * @version         1.1
  * @description     This class provides functions to generate a Spreadsheet
  *                  like static Table grouped along the Rows and Columns. It works
  *                  just like the HandsOnTable JS except that it keeps things
@@ -10,28 +10,43 @@
  *                  grouped along the Rows and Columns.
  * 
  * 
- * @param   container       A DIV Element that will hold the dynamically generated Table.
- * @param   colHeaders      A String Array containing the Column Headers.
- * @param   rowHeaders      A String Array containing the Row Headers.
- * @param   data            A 2D Array containing the data to generate the Table.
- * @param   cellRenderer    A Function that will be invoked on each Cell being added to the Table.
- *                          This allows developers to write custom logic say for eg. 
- *                          "Cell Formatting" before adding the Cell to the Table. The function
- *                          will receive 3 Paramters:
+ * @param   container       A DIV element that will wrap the dynamically generated Table.
+ * 
+ * @param   data            A JS Object with the following attributes:
+ *                          @attribute  rows            A 2D Array containing the data to generate the Table.
+ *                          @attribute  colHeaders      A String Array containing the Column Headers.
+ *                          @attribute  rowHeaders      A String Array containing the Row Headers.
+ * 
+ * @param   options         A JS Object with the following attributes:
+ *                          @attribute  caption         A Caption for the Table.
+ * 
+ *                          @attribute  appendTable     A Boolean representing if the new Table should be 
+ *                                                      appended to the Container alongside the existing Tables.
+ * 
+ *                          @attribute  cellRenderer    A Function that will be invoked on each Cell being added
+ *                                                      to the Table. This allows developers to write custom logic
+ *                                                      say for eg. "Cell Formatting" before adding the Cell to
+ *                                                      the Table. The function will receive 3 Paramters:
+ * 
+ *                                                      @param      td              The Current Cell in context.
+ *                                                      @param      isDataCell      A Boolean representing if the Cell is a Data Cell or not.
+ *                                                      @param      isFirstRow      A Boolean representing if the Cell is a part of the First Row.
+ *                                                      @param      isColHeader     A Boolean representing if the Cell is a part of the Column Header.
+ *                                                      @param      isRowHeader     A Boolean representing if the Cell is a part of the Row Header.
  *                          
- *                          @param      td              The Current Cell in context.
- *                          @param      isDataCell      A Boolean representing if the Cell is a Data Cell or not.
- *                          @param      isFirstRow      A Boolean representing if the Cell is a part of the First Row.
- *                          
- * @param   callback        A Function that will be invoked after generating the entire Table.
+ *                          @attribute  callback        A Function that will be invoked after generating the entire Table.
  */
-var Table = function( container, colHeaders, rowHeaders, data, cellRenderer, callback ) {
-    this.data         = data;
-    this.callback     = callback;
+var Table = function( container, data, options ) {
     this.container    = container;
-    this.colHeaders   = colHeaders;
-    this.rowHeaders   = rowHeaders;
-    this.cellRenderer = cellRenderer;
+    
+    this.data         = data ? data.rows        : null;
+    this.colHeaders   = data ? data.colHeaders  : null;
+    this.rowHeaders   = data ? data.rowHeaders  : null;
+    
+    this.caption      = options ? options.caption       : null;
+    this.callback     = options ? options.callback      : null;
+    this.appendTable  = options ? options.appendTable   : null;
+    this.cellRenderer = options ? options.cellRenderer  : null;
 
     this.classNames = {
         classColHeader : "col-header",
@@ -41,12 +56,13 @@ var Table = function( container, colHeaders, rowHeaders, data, cellRenderer, cal
     };
 
     this.tags = {
-        table : "table",
-        thead : "thead",
-        tbody : "tbody",
-        headC : "th",
-        row   : "tr",
-        cell  : "td"
+        caption : "caption",
+        table   : "table",
+        thead   : "thead",
+        tbody   : "tbody",
+        headC   : "th",
+        row     : "tr",
+        cell    : "td"
     };
 };
 
@@ -69,17 +85,19 @@ Table.prototype.destroy = function() {
  * generate the Table based on the Data provided.
  */
 Table.prototype.generate = function() {
-    var self = this;
+    var self  = this;
     
     /**
      * Remove any existing tables from the
      * container.
      */
-    var existingTable = self.container.getElementsByTagName( self.tags.table );
-
-    if( existingTable.length > 0 ) {
-        for( var i = 0; i < existingTable.length; i++ ) {
-            self.container.removeChild( existingTable[i] );
+    if( !this.appendTable ) {
+        var existingTable = self.container.getElementsByTagName( self.tags.table );
+    
+        if( existingTable.length > 0 ) {
+            for( var i = 0; i < existingTable.length; i++ ) {
+                self.container.removeChild( existingTable[i] );
+            }
         }
     }
 
@@ -88,6 +106,16 @@ Table.prototype.generate = function() {
      */
     var table = document.createElement( self.tags.table );
     table.className = self.classNames.classTable;
+    
+    /**
+     * Create a Caption for the Table if available
+     * and append it to the Table.
+     */
+    if( this.caption !== null ) {
+        var caption = document.createElement( self.tags.caption );
+        caption.innerHTML = this.caption;
+        table.appendChild( caption );
+    }
     
     /**
      * Create the Head Tag for the Table
@@ -135,8 +163,11 @@ Table.prototype.generate = function() {
                 /**
                  * If a cellRenderer function is available then
                  * invoke the same on the newly created Cell.
+                 * 
+                 * Pass to the function a value of "true" if this
+                 * happens to be the part of the Column Header. 
                  */
-                cell = self.cellRenderer( cell, false, false );
+                cell = self.cellRenderer( cell, false, false, true, false );
             }
 
             /**
@@ -183,9 +214,12 @@ Table.prototype.generate = function() {
                 /**
                  * If a cellRenderer function is available then
                  * invoke the same on the newly created Cell.
+                 * 
+                 * Pass to the function a value of "true" if this
+                 * happens to be the part of the Row Header. 
                  */
                 if( self.cellRenderer !== null ) {
-                    cell = self.cellRenderer( cell, false, false );
+                    cell = self.cellRenderer( cell, false, false, false, true );
                 }
 
                 /**
@@ -207,7 +241,7 @@ Table.prototype.generate = function() {
                  * Pass to the function a value of "true" if this
                  * happens to be the part of the first row.
                  */
-                cell = self.cellRenderer( cell, true, ( i === 0 ? true : false ), false );
+                cell = self.cellRenderer( cell, true, ( i === 0 ? true : false ), false, false );
             }
             
             /**
